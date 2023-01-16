d5aae03a6a9d821e56b1823b8e7de96bbd875f8e
Smethan <ethanhargus@gmail.com>
2023-01-16 17:15:12 -0600

    added everything

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import json as js
 
-
-def initialize():
-    mapFile = js.load(open("map.json"))
-    gameLoop()
+gameOver = False
 
 
 def gameLoop():
-    gameOver = False
+    mapFile = js.load(open("map.json"))
+    location = "Intro"
     while gameOver != True:
+        location = tick(mapFile[location])
+    if input("Would you like to restart? (Yes or No): ") == "Yes":
+        gameLoop()
+    else:
         return
 
 
+def tick(object):
+    print("\n", object["text"], "\n")
+    if object["choices"].count("gameOver") > 0:
+        global gameOver
+        gameOver = True
+        return
+    for idx, i in enumerate(object["choices"]):
+        print(f"{idx + 1}. {i}")
+    value = input("\nEnter your choice (case sensitive): ")
+    while object["choices"].count(value) == 0:
+        value = input("Invalid input, try again: ")
+    return value
+
+
 if __name__ == "__main__":
-    initialize()
+    gameLoop()
