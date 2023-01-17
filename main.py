703839856f03670d61ee05c31a984e13ff841b2b
Smethan <ethanhargus@gmail.com>
2023-01-17 09:41:22 -0600

    refactor into class for scoping issues

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json as js
 import logging
 
-gameOver = False
-
 logging.basicConfig(
     filename="log.txt",
     filemode="a",
@@ -12,36 +10,37 @@ logging.basicConfig(
 )
 
 
-def gameLoop():
-    global gameOver
-    mapFile = js.load(open("map.json"))
-    logging.info(f"Map loaded, {len(mapFile)} node(s) loaded.")
-    location = "Intro"
-    while gameOver != True:
-        location = step(mapFile[location])
-        logging.info(f"loading location {location}")
-    if input("Would you like to restart? (Yes or No): ").lower() == "yes":
-        logging.warning("restarting game")
-        gameOver = False
-        gameLoop()
-    else:
-        logging.warning("exiting game")
-        return
+class Game:
+    def __init__(self):
+        self.gameOver = False
+        self.location = "Intro"
 
+    def gameLoop(self):
+        mapFile = js.load(open("map.json"))
+        logging.info(f"Map loaded, {len(mapFile)} node(s) loaded.")
+        while self.gameOver != True:
+            self.location = self.step(mapFile[self.location])
+            logging.info(f"loading location {self.location}")
+        if input("Would you like to restart? (Yes or No): ").lower() == "yes":
+            logging.warning("restarting game")
+            self.gameOver = False
+            self.gameLoop()
+        else:
+            logging.warning("exiting game")
+            return
 
-def step(object):
-    print("\n", object["text"], "\n")
-    if object["choices"].count("gameOver") > 0:
-        global gameOver
-        gameOver = True
-        return
-    for idx, i in enumerate(object["choices"]):
-        print(f"{idx + 1}. {i}")
-    value = input("\nEnter your choice (Case Sensitive): ")
-    while object["choices"].count(value) == 0:
-        value = input("Invalid input, try again: ")
-    return value
+    def step(self, object):
+        print("\n", object["text"], "\n")
+        if object["choices"].count("gameOver") > 0:
+            self.gameOver = True
+            return
+        for idx, i in enumerate(object["choices"]):
+            print(f"{idx + 1}. {i}")
+        value = input("\nEnter your choice (Case Sensitive): ")
+        while object["choices"].count(value) == 0:
+            value = input("Invalid input, try again: ")
+        return value
 
 
 if __name__ == "__main__":
-    gameLoop()
+    Game().gameLoop()
